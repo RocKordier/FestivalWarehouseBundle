bcce5131fc8924091ac8316151c4aa891b2d5dc4
Eric Hertwig <eric@rocken-am-brocken.de>
2016-05-03 22:54:16 +0200

    FWB-1 add oro autoloading for bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RocKordier\Bundle\FestivalWarehouseBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Class Configuration
+ * @package RocKordier\Bundle\FestivalWarehouseBundle\DependencyInjection
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('rockordier_festivalwarehousebundle');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}
---
+++ FestivalWarehouseBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace RocKordier\Bundle\FestivalWarehouseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class RocKordierFestivalWarehouseBundle
+ * @package RocKordier\Bundle\FestivalWarehouseBundle
+ */
+class FestivalWarehouseBundle extends Bundle
+{
+
+}
