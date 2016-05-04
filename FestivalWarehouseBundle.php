014223300efb80b7727d0fa31f9c1afb3b173ed0
RocKordier <eric@ehdev.de>
2016-05-04 08:12:08 +0200

    FWB-1 Codestyle

--- FestivalWarehouseBundle.php
+++ FestivalWarehouseBundle.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Class RocKordierFestivalWarehouseBundle
+ *
  * @package RocKordier\Bundle\FestivalWarehouseBundle
  */
 class FestivalWarehouseBundle extends Bundle
