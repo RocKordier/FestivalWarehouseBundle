aca93f068d3b4c43cb6d12619b1c4211ecdcf821
RocKordier <eric@ehdev.de>
2016-05-17 06:47:56 +0200

    FWB-3 load service configuration

--- DependencyInjection/FestivalWarehouseExtension.php
+++ DependencyInjection/FestivalWarehouseExtension.php
@@ -20,11 +20,11 @@ class FestivalWarehouseExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-//        $config = $this->processConfiguration($configuration, $configs);
-//
-//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-//        $loader->load('services.yml');
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
 //        $loader->load('form.yml');
-////        $loader->load('navigation.yml');
+//        $loader->load('navigation.yml');
     }
 }
